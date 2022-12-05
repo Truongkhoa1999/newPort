52c593cd49b651d8c1c15e1a119f2898dca3549e
Truongkhoa1999 <tdkhoa.aqua@gmail.com>
2022-12-05 17:45:14 +0200

    second

--- main.js
+++ main.js
@@ -1,25 +1,192 @@
 "use strict";
 
 const scale = (a, b, c, d, e) => {
-  return (a - b) * (e - d) / (c - b) + d;
+    return (a - b) * (e - d) / (c - b) + d;
 };
 const text1 = document.getElementById('text-1');
 const text2 = document.getElementById('text-2');
 const crazy = e => {
-  const x = e.clientX || e.touches[0].clientX;
-  TweenMax.to(text1, .6, {
-    attr: {
-      startOffset: `${scale(x, 0, window.innerWidth, 0, 75)}%`
-    },
-    ease: Power3.easeOut
-  });
-  TweenMax.to(text2, .8, {
-    attr: {
-      startOffset: `${scale(x, 0, window.innerWidth, 75, 0)}%`
-    },
-    ease: Power3.easeOut
-  });
+    const x = e.clientX || e.touches[0].clientX;
+    TweenMax.to(text1, .6, {
+        attr: {
+            startOffset: `${scale(x, 0, window.innerWidth, 0, 75)}%`
+        },
+        ease: Power3.easeOut
+    });
+    TweenMax.to(text2, .8, {
+        attr: {
+            startOffset: `${scale(x, 0, window.innerWidth, 75, 0)}%`
+        },
+        ease: Power3.easeOut
+    });
 };
 ['mousemove', 'touchstart', 'touchmove'].forEach(e => {
-  window.addEventListener(e, crazy);
+    window.addEventListener(e, crazy);
 });
+
+// frame 2
+const name_item = document.querySelectorAll('.name_item');
+const search_item = document.getElementById('search-item');
+const element_projects = document.querySelectorAll('.element_projects');
+const filter_button = document.querySelectorAll('#filter_button .btn');
+const h1_price = document.querySelectorAll('h1.type');
+
+search_item.addEventListener('keyup', getItem);
+search_item.addEventListener('click', reset);
+
+function reset() {
+    for (let i = 0; i < filter_button.length; i++) {
+        filter_button[i].classList.remove('active');
+    }
+    Array.from(element_projects).forEach(function (element) {
+        element.style.display = 'block';
+    })
+    filter_button[0].classList.add('active');
+
+}
+// search item
+var h1Text = document.getElementById('showtext');
+
+const infor = document.querySelectorAll('.infor');
+function getItem(event) {
+
+    let lower_item = search_item.value.toLowerCase();
+    Array.from(infor).forEach(function (cake) {
+        let item_Name = cake.firstElementChild.textContent;
+        if (item_Name.toLowerCase().indexOf(lower_item) != -1) {
+            cake.parentElement.style.display = 'block';
+        }
+        else {
+            cake.parentElement.style.display = 'none';
+
+        }
+    })
+
+    checkNone(infor);
+}
+
+// function check if all infor[] has display none => textContent = 'can't find'
+function checkNone(target_element) {
+    var sum = 0;
+    for (let i = 0; i < target_element.length; i++) {
+        if (target_element[i].parentElement.style.display === 'block') {
+            sum++;
+        }
+    }
+    if (sum == 0) {
+        h1Text.textContent = "can't find.";
+    }
+    else {
+        h1Text.textContent = "";
+    }
+}
+
+// filter
+Array.from(filter_button).forEach(function (button) {
+    button.addEventListener('click', function (event) {
+
+        for (let i = 0; i < filter_button.length; i++) {
+            filter_button[i].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        let buttonAttr = event.target.dataset.filter;
+        // let buttonAttr = button.getAttribute('data-filter');
+        Array.from(element_projects).forEach(function (element) {
+            let elementAttr = element.dataset.item; // lấy giá trị data-* dùng getAttribute hoặc dataset
+            if (buttonAttr === elementAttr || buttonAttr === 'all') {
+                element.style.display = 'block';
+            }
+            else {
+                element.style.display = 'none';
+            }
+        })
+    })
+})
+
+
+
+// function reverseString(str){
+//     return str.split("").reverse().join("");
+// }
+
+// modal box
+
+const arrayImg = ['cake-1', 'cake-2', 'cake-3', 'cupcake-1', 'cupcake-2', 'cupcake-3',
+    'doughnut-1', 'doughnut-2', 'doughnut-3', 'sweets-1', 'sweets-2', 'sweets-3'];
+const modal_div = document.getElementById('modal');
+const imgBox = document.querySelector('.lightbox');
+
+let pos = 0;
+Array.from(element_projects).forEach(function (cake) {
+    cake.addEventListener('click', function () {
+        let srcImg = this.firstElementChild.getAttribute('src');
+
+        for (let i = 0; i < arrayImg.length; i++) {
+            if (srcImg.indexOf(arrayImg[i]) != -1) {
+                modal_div.style.display = 'block';
+                imgBox.style.backgroundImage = `url(images/${arrayImg[i]}.jpeg)`;
+                imgBox.style.backgroundSize = 'cover';
+                imgBox.style.backgroundPosition = 'center';
+                pos = i;
+            }
+        }
+        // let imgThis = `images/${}.jpeg`; 
+    })
+})
+// button modal box
+const leftBt = document.querySelector('.fa-caret-left');
+const rightBt = document.querySelector('.fa-caret-right');
+
+leftBt.addEventListener('click', slidePre);
+function slidePre() {
+    pos--;
+    if (pos < 0) {
+        pos = arrayImg.length - 1;
+    }
+    imgBox.style.backgroundImage = `url(images/${arrayImg[pos]}.jpeg)`;
+}
+rightBt.addEventListener('click', slideNext);
+function slideNext() {
+    pos++;
+    if (pos > arrayImg.length - 1) {
+        pos = 0;
+    }
+    imgBox.style.backgroundImage = `url(images/${arrayImg[pos]}.jpeg)`;
+}
+
+modal_div.addEventListener('click', function (event) {
+    let eventButton = event.target.classList;
+    if (eventButton.contains('fa-caret-left') || eventButton.contains('fa-caret-right')) {
+        this.style.display = 'block';
+    }
+    else {
+        this.style.display = 'none';
+    }
+})
+
+// Heading changes
+
+const myInterval = setInterval(displayWord, 2000)
+function displayWord() {
+    let x = document.getElementById('frame1_heading')
+    if (x.innerText == "Hello;") {
+        x.innerText = "Ola;"
+    } else if (x.innerText == "Ola;") {
+        x.innerText = "Xin Chào;"
+    } else if (x.innerText == "Xin Chào;") {
+        x.innerText = "Moi;"
+    }
+    else if (x.innerText == "Moi;") {
+        x.innerText = "Bonjour;"
+    } else if (x.innerText == "Bonjour;") {
+        x.innerText = "Ciao;"
+    } else if (x.innerText == "Ciao;") {
+        x.innerText = "你 好;"
+    } else if (x.innerText == "你 好;") {
+        x.innerText = "Привет"
+    }
+    else {
+        x.innerText = "Hello;"
+    }
+}
